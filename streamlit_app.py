e720ee912deae2b8be92d09e5188c106231e01ed
srinu-ravulapalli <125343965+srinu-ravulapalli@users.noreply.github.com>
2023-02-16 20:09:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choicei")
 streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
@@ -39,6 +39,4 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choicei")
 
